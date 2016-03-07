f4fd5560e2a1cd29526ff83135fd3b9e85075d77
jared beckler and torrence stratton <jared.beckler@gmail.com>
2016-03-07 11:34:39 -0800

    add silex routing

--- app/app.php
+++ app/app.php
@@ -16,5 +16,29 @@
         'twig.path' => __DIR__.'/../views'
     ));
 
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.html.twig');
+    });
+
+    $app->post("/sign_up", function() use ($app) {
+        $first_name = $_POST['first_name'];
+        $last_name = $_POST['last_name'];
+        $email = $_POST['email'];
+        $username = $_POST['username'];
+        $bio = $_POST['bio'];
+        $photo = $_POST['photo'];
+        $user = new User($first_name, $last_name, $email, $username, $bio, $photo);
+        $user->save();
+        $user_projects = $user->getProjects();
+
+        return $app['twig']->render('profile.html.twig', array('user' => $user, 'projects' => $user_projects));
+    });
+
+    $app->post("/user/{id}", function($id) use ($app) {
+        $user = User::find($id);
+        $user_projects = $user->getProjects();
+        return $app['twig']->render('profile.html.twig', array('user' => $user, 'projects' => $user_projects));
+    });
+
     return $app;
 ?>
