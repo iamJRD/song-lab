c1b6be1b7197d813ed75f66b93fb661a46e6ccaf
jared beckler and torrence stratton <jared.beckler@gmail.com>
2016-03-07 14:16:40 -0800

    update files and DB

--- app/app.php
+++ app/app.php
@@ -40,5 +40,40 @@
         return $app['twig']->render('profile.html.twig', array('user' => $user, 'projects' => $user_projects));
     });
 
+    $app->get("/user/{id}/edit_profile", function($id) use ($app){
+        $user = User::find($id);
+        return $app['twig']->render('edit_profile.html.twig', array('user' => $user));
+    });
+
+    $app->patch("/user/{id}/edit_profile", function($id) use ($app){
+        $user = User::find($id);
+        $new_first_name = $_POST['new_first_name'];
+        $new_last_name = $_POST['new_last_name'];
+        $new_email = $_POST['new_email'];
+        $new_username = $_POST['new_username'];
+        $new_bio = $_POST['new_bio'];
+        $new_photo = $_POST['new_photo'];
+        $user->update($new_first_name, $new_last_name, $new_email, $new_username, $new_bio, $new_photo)
+        return $app['twig']->render('profile.html.twig', array('user' => $user, 'projects' => $user_projects));
+    });
+
+    $app->get("/user/{id}/edit_project", function($id) use ($app){
+        $user = User::find($id);
+        $project = $user->getProjects($user->getId());
+        return $app['twig']->render('edit_project.html.twig', array('user' => $user));
+    });
+
+    $app->patch("/user/{id}/edit_project", function($id) use ($app){
+        $user = User::find($id);
+        $new_title = $_POST['new_title'];
+        $new_description = $_POST['new_description'];
+        $new_genre = $_POST['new_genre'];
+        $new_resources = $_POST['new_resources'];
+        $new_lyrics = $_POST['new_lyrics'];
+        $new_type = $_POST['new_type'];
+        $user->update($new_title, $new_description, $new_genre, $new_resources, $new_lyrics, $new_type)
+        return $app['twig']->render('profile.html.twig', array('user' => $user, 'projects' => $user_projects));
+    });
+
     return $app;
 ?>
