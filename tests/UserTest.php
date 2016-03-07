601c0521ba91c1d2cacd72b30d966cd9a20e74c8
jcfix <jcfix.pdx@gmail.com>
2016-03-07 10:52:38 -0800

    Pass all get tests with dummy string for photo

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -6,13 +6,143 @@
 
     require_once "src/User.php";
 
-    $server = 'mysql:host=localhost;dbname=song_lab_test';
+    $server = 'mysql:host=localhost;dbname=songlab_test';
     $username = 'root';
     $password = 'root';
     $DB = new PDO($server, $username, $password);
 
     class UserTest extends PHPUnit_Framework_TestCase
     {
+        function testGetId()
+        {
+            //Arrange
+            $id = 1;
+            $first_name = 'Sammy';
+            $last_name = 'Singsalot';
+            $email = 'sammysinger@gmail.com';
+            $username = 'sammysinger';
+            $bio = 'Portland native with a voice like an angel. Looking for other creative types to collaborate with!';
+            $photo = 'img';
+            $test_user = new User($id, $first_name, $last_name, $email, $username, $bio, $photo);
 
+            //Act
+            $result = $test_user->getId();
+
+            //Assert
+            $this->assertEquals(1, $result);
+        }
+
+        function testGetFirstName()
+        {
+            //Arrange
+            $id = 1;
+            $first_name = 'Sammy';
+            $last_name = 'Singsalot';
+            $email = 'sammysinger@gmail.com';
+            $username = 'sammysinger';
+            $bio = 'Portland native with a voice like an angel. Looking for other creative types to collaborate with!';
+            $photo = 'img';
+            $test_user = new User($id, $first_name, $last_name, $email, $username, $bio, $photo);
+
+            //Act
+            $result = $test_user->getFirstName();
+
+            //Assert
+            $this->assertEquals('Sammy', $result);
+        }
+
+        function testGetLastName()
+        {
+            //Arrange
+            $id = 1;
+            $first_name = 'Sammy';
+            $last_name = 'Singsalot';
+            $email = 'sammysinger@gmail.com';
+            $username = 'sammysinger';
+            $bio = 'Portland native with a voice like an angel. Looking for other creative types to collaborate with!';
+            $photo = 'img';
+            $test_user = new User($id, $first_name, $last_name, $email, $username, $bio, $photo);
+
+            //Act
+            $result = $test_user->getLastName();
+
+            //Assert
+            $this->assertEquals('Singsalot', $result);
+        }
+
+        function testGetEmail()
+        {
+            //Arrange
+            $id = 1;
+            $first_name = 'Sammy';
+            $last_name = 'Singsalot';
+            $email = 'sammysinger@gmail.com';
+            $username = 'sammysinger';
+            $bio = 'Portland native with a voice like an angel. Looking for other creative types to collaborate with!';
+            $photo = 'img';
+            $test_user = new User($id, $first_name, $last_name, $email, $username, $bio, $photo);
+
+            //Act
+            $result = $test_user->getEmail();
+
+            //Assert
+            $this->assertEquals('sammysinger@gmail.com', $result);
+        }
+        function testGetUsername()
+        {
+            //Arrange
+            $id = 1;
+            $first_name = 'Sammy';
+            $last_name = 'Singsalot';
+            $email = 'sammysinger@gmail.com';
+            $username = 'sammysinger';
+            $bio = 'Portland native with a voice like an angel. Looking for other creative types to collaborate with!';
+            $photo = 'img';
+            $test_user = new User($id, $first_name, $last_name, $email, $username, $bio, $photo);
+
+            //Act
+            $result = $test_user->getUsername();
+
+            //Assert
+            $this->assertEquals('sammysinger', $result);
+        }
+
+        function testGetBio()
+        {
+            //Arrange
+            $id = 1;
+            $first_name = 'Sammy';
+            $last_name = 'Singsalot';
+            $email = 'sammysinger@gmail.com';
+            $username = 'sammysinger';
+            $bio = 'Portland native with a voice like an angel. Looking for other creative types to collaborate with!';
+            $photo = 'img';
+            $test_user = new User($id, $first_name, $last_name, $email, $username, $bio, $photo);
+
+            //Act
+            $result = $test_user->getBio();
+
+            //Assert
+            $this->assertEquals('Portland native with a voice like an angel. Looking for other creative types to collaborate with!', $result);
+        }
+
+        function testGetPhoto()
+        {
+            //Arrange
+            $id = 1;
+            $first_name = 'Sammy';
+            $last_name = 'Singsalot';
+            $email = 'sammysinger@gmail.com';
+            $username = 'sammysinger';
+            $bio = 'Portland native with a voice like an angel. Looking for other creative types to collaborate with!';
+            $photo = 'img';
+            $test_user = new User($id, $first_name, $last_name, $email, $username, $bio, $photo);
+
+            //Act
+            $result = $test_user->getPhoto();
+
+            //Assert
+            $this->assertEquals('img', $result);
+        }
     }
 ?>
