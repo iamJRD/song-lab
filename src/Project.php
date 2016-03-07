e80ca92c05a664a17a402605dab4feefb657e2db
jared beckler and torrence stratton <tlstratton@gmail.com>
2016-03-07 09:58:59 -0800

    add updated database to project files

--- src/Project.php
+++ src/Project.php
@@ -1,6 +1,23 @@
 <?php
     class Project
     {
-        
+        private $id;
+        private $title;
+        private $description;
+        private $genre;
+        private $resources;
+        private $lyrics
+        private $type;
+
+        function __construct($id = null, $title, $description, $genre, $resources, $lyrics, $type)
+        {
+            $this->id = $id;
+            $this->title = $title;
+            $this->decription = $description;
+            $this->genre = $genre;
+            $this->resources = $resources;
+            $this->lyrics = $lyrics;
+            $this->type = $type;
+        }
     }
 ?>
