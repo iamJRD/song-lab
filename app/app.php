912543a7fc7e64e8fe0530cf50766753136657b5
jared beckler and torrence stratton <tlstratton@gmail.com>
2016-03-07 16:08:17 -0800

    add preliminary JS file for password handling

--- app/app.php
+++ app/app.php
@@ -1,7 +1,7 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/User.php";
-    require_once __DIR__."/../src/Project .php";
+    require_once __DIR__."/../src/Project.php";
 
     $app = new Silex\Application();
     $server = 'mysql:host=localhost;dbname=songlab';
@@ -23,11 +23,12 @@
     $app->post("/sign_up", function() use ($app) {
         $first_name = $_POST['first_name'];
         $last_name = $_POST['last_name'];
-        $email = $_POST['email'];
+        $email = $_POST['email']; //null - editable in profile
         $username = $_POST['username'];
         $bio = $_POST['bio'];
-        $photo = $_POST['photo'];
-        $user = new User($first_name, $last_name, $email, $username, $bio, $photo);
+        $photo = $_POST['photo']; //null - upload on profile edit
+        $password = $_POST['password']; //verify via JS
+        $user = new User($first_name, $last_name, $email, $username, $bio, $photo $password);
         $user->save();
         $user_projects = $user->getProjects();
 
@@ -53,7 +54,7 @@
         $new_username = $_POST['new_username'];
         $new_bio = $_POST['new_bio'];
         $new_photo = $_POST['new_photo'];
-        $user->update($new_first_name, $new_last_name, $new_email, $new_username, $new_bio, $new_photo)
+        $user->update($new_first_name, $new_last_name, $new_email, $new_username, $new_bio, $new_photo);
         return $app['twig']->render('profile.html.twig', array('user' => $user, 'projects' => $user_projects));
     });
 
@@ -71,7 +72,7 @@
         $new_resources = $_POST['new_resources'];
         $new_lyrics = $_POST['new_lyrics'];
         $new_type = $_POST['new_type'];
-        $user->update($new_title, $new_description, $new_genre, $new_resources, $new_lyrics, $new_type)
+        $user->update($new_title, $new_description, $new_genre, $new_resources, $new_lyrics, $new_type);
         return $app['twig']->render('profile.html.twig', array('user' => $user, 'projects' => $user_projects));
     });
 
